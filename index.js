63855fa383dde66ff5034de576f4a7abc9297f65
Alice Freda <alicefreda@gmail.com>
2017-11-13 20:02:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Write your solution here!
 const drivers = ['Milo', 'Otis', 'Garfield'];
-function destructivelyAppendDriver() {
+function destructivelyAppendDriver(name) {
   return drivers.push('Ralph');
+}
+function destructivelyPrependDriver(name) {
+  
 }
