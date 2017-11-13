c802dfd5d9629633bbd3383fd8903e13aae9d0a5
Alice Freda <alicefreda@gmail.com>
2017-11-13 20:01:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution here!
 const drivers = ['Milo', 'Otis', 'Garfield'];
 function destructivelyAppendDriver() {
-  return drivers.push('Alice');
+  return drivers.push('Ralph');
 }
